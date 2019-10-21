23d735ce8fc7cc0d69e7bdd7b0d5f0786c261d43
pneumaier1 <55095827+pneumaier1@users.noreply.github.com>
2019-10-21 02:24:44 -0400

    1.2

--- BallDemo.java
+++ BallDemo.java
@@ -2,6 +2,8 @@ import java.awt.Color;
 import java.awt.geom.*;
 import java.util.Random;
 import java.awt.*;
+import java.util.HashSet;
+import java.util.Iterator;
 /**
  * Class BallDemo - a short demonstration showing animation with the 
  * Canvas class. 
@@ -19,7 +21,9 @@ import java.awt.*;
 public class BallDemo   
 {
     private Canvas myCanvas;
-
+    private int numberOfBalls;
+    
+    
     /**
      * Create a BallDemo object. Creates a fresh canvas and makes it visible.
      */
@@ -33,22 +37,20 @@ public class BallDemo
      */
     public void bounce()
     {
-        int ground = 400;   // position of the ground line
+        int ground = 400;
 
         myCanvas.setVisible(true);
 
-        // draw the ground
+        
         myCanvas.drawLine(50, ground, 550, ground);
-
-        // crate and show the balls
+        
         BouncingBall ball = new BouncingBall(50, 50, 16, Color.BLUE, ground, myCanvas);
         ball.draw();
         BouncingBall ball2 = new BouncingBall(70, 80, 20, Color.RED, ground, myCanvas);
         ball2.draw();
-
-        // make them bounce
         boolean finished =  false;
-        while(!finished) {
+        while(!finished) 
+        {
             myCanvas.wait(50);           // small delay
             ball.move();
             ball2.move();
@@ -65,5 +67,51 @@ public class BallDemo
         Rectangle box = new Rectangle(50,50,400,400);
         myCanvas.draw(box);
         Random random = new Random();
+        HashSet<BoxBall> balls = new HashSet<BoxBall>();
+
+        for(int i=0; i<numberOfBalls; i++) 
+        {
+
+            Dimension size = myCanvas.getSize();
+            
+            int x = (int) box.getX() + random.nextInt((int) box.getWidth() - 16);            
+            int y = (int) box.getY() + random.nextInt((int) box.getHeight() - 16);
+            int xSpeed = random.nextInt(30);
+            int ySpeed = random.nextInt(30);
+            Color colorMixed = new Color(random.nextInt(256),random.nextInt(256),random.nextInt(256));
+            BoxBall ball = new BoxBall(x, y, xSpeed, ySpeed, 16, Color.blue, box, myCanvas);
+            balls.add(ball);
+            ball.draw();
+        }       
+        boolean finished = false;
+        while(!finished) 
+        {
+            myCanvas.wait(40);           // small delay
+            Iterator<BoxBall> it = balls.iterator();
+
+            finished = true;
+            while(it.hasNext()) {
+                BoxBall ball = it.next();
+                ball.move();
+                // stop only once all balls has stopped moving
+                if(ball.isMoving()) {
+                    finished = false;
+                }
+            }
+        }
+        Iterator<BoxBall> it = balls.iterator();
+        while(it.hasNext()) 
+        {
+            BoxBall ball = it.next();
+            ball.erase();
+        }
+    } 
+
+    public void drawFrame() 
+    {
+        int borderSize = 20;
+        Dimension size = myCanvas.getSize();
+        Rectangle r = new Rectangle(borderSize, borderSize, (int) size.getWidth() - 2*borderSize, (int) size.getHeight() - 2*borderSize);
+        myCanvas.draw(r);
     }
 }
